6ed9c72068b0405be4a46a89e64714db64d61461
Sander Mak <sander.mak@luminis.eu>
2019-04-26 14:29:22 +0200

    Also apply sortedset to begin

--- 03-libraryupdates/01-optional-stream-begin/src/whatsnew/langandlib/Book.java
+++ 03-libraryupdates/01-optional-stream-begin/src/whatsnew/langandlib/Book.java
@@ -1,9 +1,6 @@
 package whatsnew.langandlib;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -11,10 +8,10 @@ import java.util.stream.Stream;
 public class Book {
 
     public final String title;
-    public final Set<String> authors;
+    public final SortedSet<String> authors;
     public final double price;
 
-    public Book(String title, Set<String> authors, double price) {
+    public Book(String title, SortedSet<String> authors, double price) {
         this.title = title;
         this.authors = authors;
         this.price = price;
@@ -41,10 +38,10 @@ public class Book {
     }
 
     public static Stream<Book> getBooks() {
-        return Stream.of(new Book("Java 9 Modularity", Set.of("Sander Mak", "Paul Bakker"), 33.99),
-                         new Book("Designing Data-Intensive Applications", Set.of("Martin Klepmann"), 38.99),
-                         new Book("Java 8 Lambdas", Set.of("Richard Warburton"), 33.99),
-                         new Book("Too cheap", Set.of("Richard Warburton"), 9.99));
+        return Stream.of(new Book("Java 9 Modularity", new TreeSet<>(Set.of("Sander Mak", "Paul Bakker")), 33.99),
+                         new Book("Designing Data-Intensive Applications", new TreeSet<>(Set.of("Martin Klepmann")), 38.99),
+                         new Book("Java 8 Lambdas", new TreeSet<>(Set.of("Richard Warburton")), 33.99),
+                         new Book("Too cheap", new TreeSet<>(Set.of("Anonymous")), 9.99));
     }
 
     public static Book getBook() {
